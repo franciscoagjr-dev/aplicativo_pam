cbf23fac480f1fad635bfced6572e7256eb0c16b
pcetec <pc@etec.com>
2026-09-21 22:43:05 -0300

    imc

--- app/src/main/java/com/example/aplicativopam/MainActivity.kt
+++ app/src/main/java/com/example/aplicativopam/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.aplicativopam
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -11,19 +12,21 @@ import androidx.core.view.WindowInsetsCompat
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.aulaid)
+        setContentView(R.layout.calculadora_imc)
+
+        val editPeso = findViewById<EditText>(R.id.editPeso) // Peso
+        val editAltura = findViewById<EditText>(R.id.editAltura) // Altura
+        val Resultado = findViewById<TextView>(R.id.txtResultado)
+        val IMC = findViewById<Button>(R.id.btnCalcular)
 
 
-        val botao = findViewById<Button>(R.id.btnProxima)
-        val texto = findViewById<EditText>(R.id.editValor)
+        IMC.setOnClickListener {
+
+            val imc =((editAltura.text.toString().toFloat() * editAltura.text.toString().toFloat())/editPeso.text.toString().toFloat())
+
+            Resultado.text ="O IMC é:" + imc
 
 
-
-        botao.text = "oi testando o botão"
-
-        botao.setOnClickListener {
-            botao.text = texto.text
-            }
         }
     }
 }
